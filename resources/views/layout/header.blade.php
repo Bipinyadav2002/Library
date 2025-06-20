2239e3684768ef53fdcf4dfdbbd3d798c2f6cb6f
Bipin Yadav <bipinyadav5489@gmail.com>
2025-06-20 16:16:52 +0530

    changes in all sidebar and dashboard

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,24 +1,24 @@
 <header class="top-header">
-      <nav class="navbar navbar-expand gap-3">
+    <nav class="navbar navbar-expand gap-3">
         <div class="mobile-toggle-icon fs-3">
             <i class="bi bi-list"></i>
-          </div>
-          <form class="searchbar">
-              <div class="position-absolute top-50 translate-middle-y search-icon ms-3"><i class="bi bi-search"></i></div>
-              <input class="form-control" type="text" placeholder="Type here to search">
-              <div class="position-absolute top-50 translate-middle-y search-close-icon"><i class="bi bi-x-lg"></i></div>
-          </form>
-          <div class="top-navbar-right ms-auto">
+        </div>
+        <form class="searchbar">
+            <div class="position-absolute top-50 translate-middle-y search-icon ms-3"><i class="bi bi-search"></i></div>
+            <input class="form-control" type="text" placeholder="Type here to search">
+            <div class="position-absolute top-50 translate-middle-y search-close-icon"><i class="bi bi-x-lg"></i></div>
+        </form>
+        <div class="top-navbar-right ms-auto">
             <ul class="navbar-nav align-items-center">
-              <li class="nav-item search-toggle-icon">
-                <a class="nav-link" href="#">
-                  <div class="">
-                    <i class="bi bi-search"></i>
-                  </div>
-                </a>
-            </li>
+                <li class="nav-item search-toggle-icon">
+                    <a class="nav-link" href="#">
+                        <div class="">
+                            <i class="bi bi-search"></i>
+                        </div>
+                    </a>
+                </li>
 
-            <li class="nav-item dropdown dropdown-large">
+                {{-- <li class="nav-item dropdown dropdown-large">
               <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
                 <div class="projects">
                   <i class="bi bi-grid-3x3-gap-fill"></i>
@@ -340,82 +340,90 @@
                    </a>
                </div>
               </div>
-            </li>
-            <li class="nav-item dropdown dropdown-user-setting">
-              <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
-                <div class="user-setting d-flex align-items-center">
-                  <img src="{{url('assets/images/avatars/avatar-1.png')}}" class="user-img" alt="">
-                </div>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-end">
-                <li>
-                   <a class="dropdown-item" href="#">
-                     <div class="d-flex align-items-center">
-                        <img src="{{url('assets/images/avatars/avatar-1.png')}}" alt="" class="rounded-circle" width="54" height="54">
-                        <div class="ms-3">
-                          <h6 class="mb-0 dropdown-user-name">Jhon Deo</h6>
-                          <small class="mb-0 dropdown-user-designation text-secondary">HR Manager</small>
+            </li> --}}
+                <li class="nav-item dropdown dropdown-user-setting">
+                    <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#"
+                        data-bs-toggle="dropdown">
+                        <div class="user-setting d-flex align-items-center">
+                            <img src="{{ url('assets/images/avatars/avatar-1.png') }}" class="user-img" alt="">
                         </div>
-                     </div>
-                   </a>
-                 </li>
-                 <li><hr class="dropdown-divider"></li>
-                 <li>
-                    <a class="dropdown-item" href="pages-user-profile.html">
-                       <div class="d-flex align-items-center">
-                         <div class=""><i class="bi bi-person-fill"></i></div>
-                         <div class="ms-3"><span>Profile</span></div>
-                       </div>
-                     </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                       <div class="d-flex align-items-center">
-                         <div class=""><i class="bi bi-gear-fill"></i></div>
-                         <div class="ms-3"><span>Setting</span></div>
-                       </div>
-                     </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="index2.html">
-                       <div class="d-flex align-items-center">
-                         <div class=""><i class="bi bi-speedometer"></i></div>
-                         <div class="ms-3"><span>Dashboard</span></div>
-                       </div>
-                     </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                       <div class="d-flex align-items-center">
-                         <div class=""><i class="bi bi-piggy-bank-fill"></i></div>
-                         <div class="ms-3"><span>Earnings</span></div>
-                       </div>
-                     </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                       <div class="d-flex align-items-center">
-                         <div class=""><i class="bi bi-cloud-arrow-down-fill"></i></div>
-                         <div class="ms-3"><span>Downloads</span></div>
-                       </div>
-                     </a>
-                  </li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li>
-                    <a class="dropdown-item"  href="#" onclick="event.preventDefault(); document.getElementById('admin-logout-form').submit();">
-                       <div class="d-flex align-items-center">
-                         <div class=""><i class="bi bi-lock-fill"></i></div>
-                         <div class="ms-3"><span>Logout</span></div>
-                       </div>
-                     </a>
-                      <form id="admin-logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
-                       @csrf
-                        </form>
-                  </li>
-              </ul>
-            </li>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" style="background-color: #000; color: #fff;">
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <div class="d-flex align-items-center">
+                                    <img src="{{ url('assets/images/avatars/avatar-1.png') }}" alt=""
+                                        class="rounded-circle" width="54" height="54">
+                                    <div class="ms-3">
+                                        <h6 class="mb-0 dropdown-user-name">Jhon Deo</h6>
+                                        <small class="mb-0 dropdown-user-designation text-secondary">HR Manager</small>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="pages-user-profile.html">
+                                <div class="d-flex align-items-center">
+                                    <div class=""><i class="bi bi-person-fill"></i></div>
+                                    <div class="ms-3"><span>Profile</span></div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <div class="d-flex align-items-center">
+                                    <div class=""><i class="bi bi-gear-fill"></i></div>
+                                    <div class="ms-3"><span>Setting</span></div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="index2.html">
+                                <div class="d-flex align-items-center">
+                                    <div class=""><i class="bi bi-speedometer"></i></div>
+                                    <div class="ms-3"><span>Dashboard</span></div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <div class="d-flex align-items-center">
+                                    <div class=""><i class="bi bi-piggy-bank-fill"></i></div>
+                                    <div class="ms-3"><span>Earnings</span></div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <div class="d-flex align-items-center">
+                                    <div class=""><i class="bi bi-cloud-arrow-down-fill"></i></div>
+                                    <div class="ms-3"><span>Downloads</span></div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#"
+                                onclick="event.preventDefault(); document.getElementById('admin-logout-form').submit();">
+                                <div class="d-flex align-items-center">
+                                    <div class=""><i class="bi bi-lock-fill"></i></div>
+                                    <div class="ms-3"><span>Logout</span></div>
+                                </div>
+                            </a>
+                            <form id="admin-logout-form" action="{{ route('admin.logout') }}" method="POST"
+                                style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </li>
             </ul>
-            </div>
-      </nav>
-    </header>
-     <!--end top header-->
+        </div>
+    </nav>
+</header>
+<!--end top header-->
--- resources/views/layout/masterlayout.blade.php
+++ resources/views/layout/masterlayout.blade.php
@@ -60,7 +60,7 @@
 
        <!--start switcher-->
        <div class="switcher-body">
-        <button class="btn btn-primary btn-switcher shadow-sm" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="bi bi-paint-bucket me-0"></i></button>
+        {{-- <button class="btn btn-primary btn-switcher shadow-sm" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="bi bi-paint-bucket me-0"></i></button> --}}
         <div class="offcanvas offcanvas-end shadow border-start-0 p-2" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling">
           <div class="offcanvas-header border-bottom">
             <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Theme Customizer</h5>
